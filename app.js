3c4892d25c87d2ec92fc73a182fa66511004af79
sanjarsanakulov <sanjarbek.sl@gmail.com>
2022-02-04 16:00:45 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+console.log(0 == false);
+
+console.log('Hi. I am Sanjar and I learn github');
+
+let a = 3,
+    b = 4;
+console.log(a + b);
+
+console.log('Bu endi git tool box ni tekshirish uchun test');
+
+let g = 5,
+    t = 6,
+    c = 9;
+
+console.log(g + t -c);
