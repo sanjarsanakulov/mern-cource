0b0e46ed13103ee94f8f185dcd64a2cbdcdccfe5
sanjarsanakulov <sanjarbek.sl@gmail.com>
2022-02-04 16:49:36 +0500

    app and index files edit

--- app.js
+++ app.js
@@ -1,13 +1,3 @@
-console.log(0 == false);
-
-console.log('Hi. I am Sanjar and I learn github');
-
-let a = 3,
-    b = 4;
-console.log(a + b);
-
-console.log('Bu endi git tool box ni tekshirish uchun test');
-
 let g = 5,
     t = 6,
     c = 9;
