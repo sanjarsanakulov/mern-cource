b7fbe0b8450231d69c3f61c4c6290e1b584ccc9f
sanjar <sanjarbek.sl@gmail.com>
2022-02-20 23:30:29 +0500

    3-amaliyot uydan

--- app.js
+++ app.js
@@ -1,15 +1,12 @@
 "use strict";
 
 /* Darsga topshiriq:
-1) Tsiklni yordamida foydalanuvchiga yangiliklar haqidagi savollarni avtomatlashtirish
-2) Foydalanuvchi javobni bo'sh satr sifatida qoldirmasligi uchun shunday qiling,
-javobni bekor qilish yoki 50 belgidan uzunroq yangilik nomini kiritish. Agar bu sodir bo'lsa -
-foydalanuvchini yana savollarga qaytaring. (Har qanday satrga quyidagicha kirish mumkin
-str.length - va uzunligini oling)
-3) Shartlardan foydalanib, personalMovieDB.count-ni tekshiring, agar u 10 dan kam bo'lsa - xabarni ko'rsating
-"Juda oz sonli yangiliklar o'qilibdi", agar 10 dan 30 gacha bo'lsa - "Siz klassik tomoshabinsiz", agar ko'proq bo'lsa -
-- Siz yangilklar ishqibozisiz. Va agar u biron bir variantga mos kelmasa - "Xato yuz berdi"
-4) Amaliy mashg'ulot va tsiklni yana ikkita usulda qayta yozing  */
+1) Darsga muvofiq topshiriqning birinchi qismini takrorlang
+2) Privat xususiyatlarini tekshiradigan showMyDB funktsiyasini yarating. Agar pozitsiya false turgan
+bo'lsa dasturning asosiy ob'ektini konsolga ko'rsatadi
+3) writeYourGenres funktsiyasini yarating, unda foydalanuvchi savolga 3 marta javob beradi
+"Sizning sevimli janringiz - ${ketma-ket raqam}". Har bir javob ma'lumotlar genres qatoriga yoziladi
+P.S. Funksiyalarni chaqirish shart emas */
 
 const numberOfNews = +prompt("Siz qancha yangilik ko'rdingiz?");
 
@@ -19,14 +16,14 @@ const personalNewsDB = {
   actors: {},
   genres: [],
   privat: false,
-}
+};
 
 for (let i = 0; i < 2; i++) {
   const a = prompt("Oxirgi ko'rgan yangiliklarizdan biri?"),
-      b = prompt("Unga qancha baho bergan bo'lar edingiz?");
+    b = prompt("Unga qancha baho bergan bo'lar edingiz?");
 
   if (a != null && b != null && a != "" && b != "" && a.length < 50) {
-    personalNewsDB.news[a] = b; 
+    personalNewsDB.news[a] = b;
     console.log("Tayyor");
   } else {
     console.log("Error");
