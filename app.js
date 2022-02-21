f7fe34efe1757f7725e65eb2f481c7f7a103677d
sanjarsanakulov <sanjarbek.sl@gmail.com>
2022-02-21 11:53:30 +0500

    3-amaliyot oxirigacha officedan

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 /* Darsga topshiriq:
 1) Darsga muvofiq topshiriqning birinchi qismini takrorlang
@@ -8,37 +8,66 @@ bo'lsa dasturning asosiy ob'ektini konsolga ko'rsatadi
 "Sizning sevimli janringiz - ${ketma-ket raqam}". Har bir javob ma'lumotlar genres qatoriga yoziladi
 P.S. Funksiyalarni chaqirish shart emas */
 
-const numberOfNews = +prompt("Siz qancha yangilik ko'rdingiz?");
+let numberOfNews;
+
+function startProject() {
+    numberOfNews = +prompt("Siz qancha yangilik ko'rdingiz?");
+
+    while (numberOfNews == "" || numberOfNews == null || isNaN(numberOfNews)) {
+        numberOfNews = +prompt("Siz qancha yangilik ko'rdingiz?");
+    }
+}
+startProject();
 
 const personalNewsDB = {
-  count: numberOfNews,
-  news: {},
-  actors: {},
-  genres: [],
-  privat: false,
+    count: numberOfNews,
+    news: {},
+    actors: {},
+    genres: [],
+    privat: false,
 };
 
-for (let i = 0; i < 2; i++) {
-  const a = prompt("Oxirgi ko'rgan yangiliklarizdan biri?"),
-    b = prompt("Unga qancha baho bergan bo'lar edingiz?");
+function rememberNews() {
+    for (let i = 0; i < 2; i++) {
+        const a = prompt("Oxirgi ko'rgan yangiliklarizdan biri?"),
+            b = prompt("Unga qancha baho bergan bo'lar edingiz?");
 
-  if (a != null && b != null && a != "" && b != "" && a.length < 50) {
-    personalNewsDB.news[a] = b;
-    console.log("Tayyor");
-  } else {
-    console.log("Error");
-    i--;
-  }
+        if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+            personalNewsDB.news[a] = b;
+            console.log("Tayyor");
+        } else {
+            console.log("Error");
+            i--;
+        }
+    }
 }
+rememberNews();
 
-console.log(personalNewsDB);
-
-if (personalNewsDB.count < 10) {
-  console.log("Juda oz sonli yangiliklar o'qilibdi");
-} else if (9 < personalNewsDB.count && personalNewsDB.count < 30) {
-  console.log("Siz klassik tomoshabinsiz");
-} else if (29 < personalNewsDB.count && personalNewsDB.count <= 40) {
-  console.log("Siz yangiliklar ishqibozisiz");
-} else {
-  console.log("Xato yuz berdi");
+function showPersonalLevel() {
+    if (personalNewsDB.count < 10) {
+        console.log("Juda oz sonli yangiliklar o'qilibdi");
+    } else if (9 < personalNewsDB.count && personalNewsDB.count < 30) {
+        console.log("Siz klassik tomoshabinsiz");
+    } else if (29 < personalNewsDB.count && personalNewsDB.count <= 40) {
+        console.log("Siz yangiliklar ishqibozisiz");
+    } else {
+        console.log("Xato yuz berdi");
+    }
 }
+showPersonalLevel();
+
+function showMyDB(hidden) {
+    if (!hidden) {
+        console.log(personalNewsDB);
+    }
+}
+showMyDB(personalNewsDB.privat);
+
+// sikl ichida [i - 1] yozilishiga sabab sikl 1 dan(i = 1) boshlanayapti, natija chiqqanda massivning 0 index bo'sh bo'lib qoladi va 1,2,3 index deb chiqadi
+// shuni oldini olish uchun ham [i - 1] deb yozilgan va massiv o indexdan boshlab ishlagan va bo'sh qolmagan
+function writeYourGenres() {
+    for (let i = 1; i <= 3; i++) {
+        personalNewsDB.genres[i -1] = prompt(`Sizning sevimli janringiz nomeri ${i}`);
+    }
+}
+writeYourGenres();
